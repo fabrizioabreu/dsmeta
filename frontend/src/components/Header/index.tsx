3bf944ab9ad339d5820179f2f7b0262a96a0bdcf
fabrizioabreu <fabrizioabreu@live.com>
2022-10-18 16:57:49 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import icon from '../../assets/img/logo.svg'
+
+import './styles.css'
+
+function Heade(): JSX.Element {
+  return (
+    <>
+      <header>
+        <div className="dsmeta-logo-container">
+          <img src={icon} alt="DSMeta" />
+          <h1>DSMeta</h1>
+          <p>
+            Desenvolvido por
+            <a href="https://www.instagram.com/devsuperior.ig">
+              @devsuperior.ig
+            </a>
+          </p>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default Heade;
