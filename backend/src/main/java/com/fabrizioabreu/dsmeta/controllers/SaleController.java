f29debd2eb4ab41c07ba292ab1a40d156e0eb238
fabrizioabreu <fabrizioabreu@live.com>
2022-10-26 21:14:19 -0300

    Date Select

--- backend/src/main/java/com/fabrizioabreu/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/fabrizioabreu/dsmeta/controllers/SaleController.java
@@ -1,10 +1,11 @@
 package com.fabrizioabreu.dsmeta.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fabrizioabreu.dsmeta.entities.Sale;
@@ -18,7 +19,10 @@ public class SaleController {
 	private SaleService saleService;
 	
 	@GetMapping
-	public List<Sale> findaSales() {
-		return saleService.findSales();
+	public Page<Sale> findaSales(
+			@RequestParam(value = "minDate", defaultValue = "") String minDate, 
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate, 
+			Pageable pageable) {
+		return saleService.findSales(minDate, maxDate, pageable);
 	}
 }
